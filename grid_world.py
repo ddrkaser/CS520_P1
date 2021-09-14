086815456d6dfc0c06c45c68e736796c72fbff9c
Ezra Brauner <ezrasammy@gmail.com>
2021-09-14 19:14:13 -0400

    Minor improvements/changes, additional structure

--- grid_world.py
+++ grid_world.py
@@ -6,6 +6,8 @@ from queue import PriorityQueue
 # creating gridworld
 def generate_gridworld(length,width,p):
     grid = np.random.choice([0,1],length*width, [1-p,p]).reshape(length,width)
+    grid[0][0] = 0
+    grid[-1][-1] = 0
     return grid
 
 #A* Algorithm
@@ -24,10 +26,20 @@ def algorithmA(grid,start,end):
     tieBreaker=0 # it is use to break tie of cells which have same f value.
     pq.put((0,tieBreaker,start)) # add start cell to the priority Queue.
     cameFrom={} # keep track of parent of cell
-    g={eachcolumn:float("inf") for eachRow in grid for eachcolumn in eachRow}
+    g={(x, y):float("inf") for y, eachRow in enumerate(grid) for x, eachcolumn in enumerate(eachRow)}
     g[start]=0
-    f={eachcolumn:float("inf") for eachRow in grid for eachcolumn in eachRow}
+    f={(x, y):float("inf") for y, eachRow in enumerate(grid) for x, eachcolumn in enumerate(eachRow)}
     start = (0,0)
     #goal = (length,width)
-    f[start]=hureisticValue(start,goal)
-
+    f[start]=hureisticValue(start, end)
+    h = {(x, y): hureisticValue((x, y), end) for y, eachRow in enumerate(grid) for x, eachcolumn in enumerate(eachRow)}
+    parent = {}
+	
+    curr_knowledge = [[0 for i in range(len(grid))] for j in range(len(grid[0]))]
+	
+    #while True:
+        # A* per page 2 of assignment description
+		# Move pointer square by square along path
+			# If new square UNBLOCKED, update curr_knowledge. If BLOCKED, restart loop
+		
+algorithmA(generate_gridworld(5,5,.5), (0,0), (4,4))
