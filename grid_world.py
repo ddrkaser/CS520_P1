5f01868f5a6250edd7c7bc539368edc2d4713a5a
Ezra Brauner <ezrasammy@gmail.com>
2021-09-20 14:15:51 -0400

    All functions needed for Q 1-7

--- grid_world.py
+++ grid_world.py
@@ -4,7 +4,7 @@ import numpy as np
 
 # creating gridworld
 def generate_gridworld(length,width,probability):
-    grid = np.random.choice([0,1],length*width, p=[1.0-probability,probability]).reshape(length,width)
+    grid = np.random.choice([0,1],length*width, p=[1-probability,probability]).reshape(length,width)
     grid[0][0] = 0
     grid[-1][-1] = 0
     return grid
@@ -20,20 +20,17 @@ h[currentCell] is a heuristic cost from currentCell to the goal
 f[currentCell]=g[currentCell]+h[currentCell]
 """
 
-def algorithmA(grid,start,end, is_known):
-    g={(x, y):float("inf") for y, eachRow in enumerate(grid) for x, eachcolumn in enumerate(eachRow)}
+def A_star(curr_knowledge, start, end):
+    g={(x, y):float("inf") for y, eachRow in enumerate(curr_knowledge) for x, eachcolumn in enumerate(eachRow)}
     g[start]=0
-    f={(x, y):float("inf") for y, eachRow in enumerate(grid) for x, eachcolumn in enumerate(eachRow)}
+    f={(x, y):float("inf") for y, eachRow in enumerate(curr_knowledge) for x, eachcolumn in enumerate(eachRow)}
+    #goal = (length,width)
     f[start]=hureisticValue(start, end)
-    h = {(x, y): hureisticValue((x, y), end) for y, eachRow in enumerate(grid) for x, eachcolumn in enumerate(eachRow)}
+    h = {(x, y): hureisticValue((x, y), end) for y, eachRow in enumerate(curr_knowledge) for x, eachcolumn in enumerate(eachRow)}
     parent = {}
 	
     pq=set([ (f[start], start) ]) # add start cell and distance information to the priority queue.
-	
-    curr_knowledge = [[0 for i in range(len(grid))] for j in range(len(grid[0]))]
-    if is_known:
-        curr_knowledge = grid
-    print(curr_knowledge)
+    # A* algorithm, based on assignment instructions
     while True:
         n = min(pq)
         pq.remove(n)
@@ -47,11 +44,11 @@ def algorithmA(grid,start,end, is_known):
             while path_pointer != start:
                 shortest_path.append(path_pointer)
                 path_pointer = parent[path_pointer]
+            shortest_path.append(start)
             shortest_path = shortest_path[::-1]
             print(shortest_path)
-            break
+            return shortest_path
 			
-		# A* algorithm, based on assignment instructions
 		# Determine which neighbors are valid successors
         if curr_pos[0] > 0 and curr_knowledge[curr_pos[1]][curr_pos[0] - 1] == 0: # the current node has a neighbor to its left which is unblocked
             left_neighbor = (curr_pos[0] - 1, curr_pos[1])
@@ -80,15 +77,47 @@ def algorithmA(grid,start,end, is_known):
             pq.add((g[successor] + h[successor], successor))
         
         if len(pq) == 0: # if priority queue is empty at any point, then unsolvable
+            print("Not solvable")
             return False
-       
-	   # Move pointer square by square along path
+			
+def algorithmA(grid,start,end, is_known):
+    print(grid)
+    curr_knowledge = [[0 for i in range(len(grid))] for j in range(len(grid[0]))]
+    if is_known: # If the grid is considered known to the robot
+        curr_knowledge = grid
+    print(curr_knowledge)
+    
+    shortest_path = A_star(curr_knowledge, start, end)
+    print(shortest_path)
+    if not shortest_path:
+        return False
+    is_broken = False
+    while True:
+		 # Move pointer square by square along path
+        for sq in shortest_path:
+            x = sq[0]
+            y = sq[1]
+			
 			# If new square UNBLOCKED, update curr_knowledge. If BLOCKED, restart loop
-		
-#Test
-algorithmA(generate_gridworld(5,5,.5), (0,0), (4,4), True)
-#Test2
-grid = generate_gridworld(20,20,.3)
-start = (0,0)
-end = (len(grid)-1,len(grid)-1)
-path_finder = algorithmA(grid, start, end, True)
+            if grid[y][x] == 1:
+                shortest_path = A_star(curr_knowledge, prev_sq, end)
+                if not shortest_path:
+                    return False
+                is_broken = True
+                break
+            else:
+                 if x != 0:
+                     curr_knowledge[y][x - 1] = grid[y][x - 1]
+                 if x < len(curr_knowledge[0]) - 1:
+                     curr_knowledge[y][x + 1] = grid[y][x + 1]
+                 if y != 0:
+                     curr_knowledge[y - 1][x] = grid[y - 1][x]
+                 if y < len(curr_knowledge) - 1:
+                     curr_knowledge[y + 1][x] = grid[y + 1][x]
+            prev_sq = sq
+        if not is_broken:
+            break
+        is_broken = False
+    return shortest_path
+    
+shortest_path = algorithmA(generate_gridworld(5,5,.3), (0,0), (4,4), False)
