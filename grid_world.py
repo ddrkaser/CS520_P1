d7b46eaae8642d7fae457496bea0044e6ff63882
Ezra Brauner <ezrasammy@gmail.com>
2021-09-23 13:25:06 -0400

    Functionality for more questions, better comments

--- grid_world.py
+++ grid_world.py
@@ -1,40 +1,58 @@
 #!/usr/bin/env python3
 
 import numpy as np
+from math import sqrt
 
-# creating gridworld
-def generate_gridworld(length,width,probability):
-    grid = np.random.choice([0,1],length*width, p=[1-probability,probability]).reshape(length,width)
+# Generates a length * width size gridworld
+# Each square has a probability chance of being blocked
+# The start and end squares are set to unblocked
+def generate_gridworld(length, width, probability):
+    grid = np.random.choice([0,1], length * width, p = [1 - probability, probability]).reshape(length, width)
     grid[0][0] = 0
     grid[-1][-1] = 0
     return grid
 
-#A* Algorithm
-def hureisticValue(point1, point2):
+# Constants to represent various types of heuristics
+EUCLIDEAN_DIST = 0
+MANHATTAN_DIST = 1
+CHEBYSHEV_DIST = 2
+
+# Calculates h(x) using one of a range of heuristics
+def hureisticValue(point1, point2, heuristic_type):
     x1,y1=point1
     x2,y2=point2
-    return abs(x1-x2)+abs(y1-y2)
+    if heuristic_type == 0:
+        return sqrt((x1 - x2)**2 + (y1 - y2)**2)
+    elif heuristic_type == 1:
+        return abs(x1 - x2) + abs(y1 - y2)
+    else:
+        return max(abs(x1 - x2), abs(y1 - y2))
 """
 g[currentCell] is the cost from start cell to currentCell
 h[currentCell] is a heuristic cost from currentCell to the goal
 f[currentCell]=g[currentCell]+h[currentCell]
 """
 
+# Defines steps for running the A* algorithm once
 def A_star(curr_knowledge, start, end):
-    g={(x, y):float("inf") for y, eachRow in enumerate(curr_knowledge) for x, eachcolumn in enumerate(eachRow)}
-    g[start]=0
-    f={(x, y):float("inf") for y, eachRow in enumerate(curr_knowledge) for x, eachcolumn in enumerate(eachRow)}
-    #goal = (length,width)
-    f[start]=hureisticValue(start, end)
-    h = {(x, y): hureisticValue((x, y), end) for y, eachRow in enumerate(curr_knowledge) for x, eachcolumn in enumerate(eachRow)}
+    # Initializes the g(x), f(x), and h(x) values for all squares
+    g = {(x, y):float("inf") for y, eachRow in enumerate(curr_knowledge) for x, eachcolumn in enumerate(eachRow)}
+    g[start] = 0
+    f = {(x, y):float("inf") for y, eachRow in enumerate(curr_knowledge) for x, eachcolumn in enumerate(eachRow)}
+    f[start] = hureisticValue(start, end, EUCLIDEAN_DIST)
+    h = {(x, y): hureisticValue((x, y), end, EUCLIDEAN_DIST) for y, eachRow in enumerate(curr_knowledge) for x, eachcolumn in enumerate(eachRow)}
     parent = {}
 	
-    pq=set([ (f[start], start) ]) # add start cell and distance information to the priority queue.
+	# Creates a priority queue using a Python set, adding start cell and its distance information
+    pq = set([ (f[start], start) ])
+	
     # A* algorithm, based on assignment instructions
     while True:
+		# Remove the node in the priority queue with the smallest f value
         n = min(pq)
         pq.remove(n)
         successors = []
+		# curr_pos is a tuple (x, y) where x represents the column the square is in, and y represents the row
         curr_pos = n[1]
 		
 		# if goal node removed from priority queue, shortest path found
@@ -76,48 +94,64 @@ def A_star(curr_knowledge, start, end):
             parent[successor] = curr_pos
             pq.add((g[successor] + h[successor], successor))
         
-        if len(pq) == 0: # if priority queue is empty at any point, then unsolvable
+		# if priority queue is empty at any point, then unsolvable
+        if len(pq) == 0:
             print("Not solvable")
             return False
-			
-def algorithmA(grid,start,end, is_known):
+
+# Handles processing of Repeated A*, restarting that algorithm if a blocked square is found in the determined shortest path
+def algorithmA(grid, start, end, is_grid_known, has_four_way_vision):
     print(grid)
+    # The assumed state of the gridworld at any point in time. For some questions, the current knowledge is unknown at the start
     curr_knowledge = [[0 for i in range(len(grid))] for j in range(len(grid[0]))]
-    if is_known: # If the grid is considered known to the robot
+    # If the grid is considered known to the robot, operate on that known grid
+	# Else, the robot assumes a completely unblocked gridworld and will have to discover it as it moves
+    if is_grid_known:
         curr_knowledge = grid
     print(curr_knowledge)
     
+	# Run A* once on grid as known, returning False if unsolvable
     shortest_path = A_star(curr_knowledge, start, end)
     print(shortest_path)
     if not shortest_path:
         return False
     is_broken = False
     while True:
-		 # Move pointer square by square along path
+		# Move pointer square by square along path
         for sq in shortest_path:
             x = sq[0]
             y = sq[1]
 			
-			# If new square UNBLOCKED, update curr_knowledge. If BLOCKED, restart loop
+			# If blocked, rerun A* and restart loop
             if grid[y][x] == 1:
+                # If the robot can only see squares in its direction of movement, update its current knowledge of the grid to include this blocked square
+                if not has_four_way_vision:
+                    curr_knowledge[y][x] = 1
+					
                 shortest_path = A_star(curr_knowledge, prev_sq, end)
                 if not shortest_path:
                     return False
                 is_broken = True
                 break
+			# If new square unblocked, update curr_knowledge. Loop will restart and move to next square on presumed shortest path
             else:
-                 if x != 0:
-                     curr_knowledge[y][x - 1] = grid[y][x - 1]
-                 if x < len(curr_knowledge[0]) - 1:
-                     curr_knowledge[y][x + 1] = grid[y][x + 1]
-                 if y != 0:
-                     curr_knowledge[y - 1][x] = grid[y - 1][x]
-                 if y < len(curr_knowledge) - 1:
-                     curr_knowledge[y + 1][x] = grid[y + 1][x]
+                 # If the robot can see in all compass directions, update squares adjacent to its current position
+                 if has_four_way_vision:
+                     if x != 0:
+                         curr_knowledge[y][x - 1] = grid[y][x - 1]
+                     if x < len(curr_knowledge[0]) - 1:
+                         curr_knowledge[y][x + 1] = grid[y][x + 1]
+                     if y != 0:
+                         curr_knowledge[y - 1][x] = grid[y - 1][x]
+                     if y < len(curr_knowledge) - 1:
+                         curr_knowledge[y + 1][x] = grid[y + 1][x]
             prev_sq = sq
         if not is_broken:
             break
         is_broken = False
     return shortest_path
     
-shortest_path = algorithmA(generate_gridworld(5,5,.3), (0,0), (4,4), False)
+# Test algorithm
+# is_grid_known is True for Questions 4, 5. It is False for later questions
+# has_four_way_vision is True for all questions except 7
+shortest_path = algorithmA(generate_gridworld(5, 5, 0.3), (0,0), (4,4), False, True)
